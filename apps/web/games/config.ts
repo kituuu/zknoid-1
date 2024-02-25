cffe0b95411c435d9cb3abb13d1dfad98e53e27c
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-02-25 23:49:16 +0300

    Fool web init

--- apps/web/games/config.ts
+++ apps/web/games/config.ts
@@ -1,10 +1,8 @@
-import { createConfig } from "@/lib/createConfig";
-import { arkanoidConfig } from "./arkanoid/config";
-import { randzuConfig } from "./randzu/config";
+import { createConfig } from '@/lib/createConfig';
+import { arkanoidConfig } from './arkanoid/config';
+import { randzuConfig } from './randzu/config';
+import { foolConfig } from './fool/config';
 
 export const zkNoidConfig = createConfig({
-    games: [
-        arkanoidConfig,
-        randzuConfig
-    ]
-})
+  games: [arkanoidConfig, randzuConfig, foolConfig],
+});
---
+++ apps/web/games/fool/components/GameView.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export const GameView = () => {
+  return <div> Initial game view </div>;
+};
