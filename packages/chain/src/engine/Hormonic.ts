2a95edef16fd857dcc03b2cae2c11e6fefdb7e42
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-02-26 13:24:14 +0300

    Added setup boilerplate

--- packages/chain/src/engine/Hormonic.ts
+++ packages/chain/src/engine/Hormonic.ts
@@ -1,1 +1,22 @@
-export {};
+import { Field, Group, PrivateKey, PublicKey } from 'o1js';
+
+/*
+    g - generator
+    pk - public key for encryption
+    m - value to encrypt
+    r - random value
+*/
+export const encrypt = (
+    g: Group,
+    pk: PublicKey,
+    m: [Group, Group],
+    r: Field
+): [Group, Group] => {
+    // Todo. For now no encryption executed
+    return m;
+};
+
+export const decrypt = (sk: PrivateKey, m: [Group, Group]): [Group, Group] => {
+    // Todo. For now no decryption executed
+    return m;
+};
--- packages/chain/src/fool/Fool.ts
+++ packages/chain/src/fool/Fool.ts
@@ -1,7 +1,7 @@
-import { State, StateMap } from '@proto-kit/protocol';
+import { Option, State, StateMap, assert } from '@proto-kit/protocol';
 import { MatchMaker, QueueListItem } from '../engine/MatchMaker';
 import { state, runtimeMethod, runtimeModule } from '@proto-kit/module';
-import { Proof, Provable, Struct, UInt64 } from 'o1js';
+import { Bool, Group, Proof, Provable, PublicKey, Struct, UInt64 } from 'o1js';
 
 const FOOL_DECK_SIZE = 36;
 
@@ -13,9 +13,118 @@ export class Deck extends Struct({
     cards: Provable.Array(Card, FOOL_DECK_SIZE),
 }) {}
 
+export class EncryptedCard extends Struct({
+    value: Group,
+}) {}
+
+export class EncryptedDeck extends Struct({
+    cards: Provable.Array(EncryptedCard, FOOL_DECK_SIZE),
+    numOfEncryptions: UInt64,
+}) {}
+
+enum GameStatus {
+    SETUP,
+    GAME,
+}
+
+export class GameInfo extends Struct({
+    status: UInt64, // change to provable type
+    deck: EncryptedDeck,
+    curPlayerIndex: UInt64, // Index of current player
+    waitDecFrom: UInt64,
+    maxPlayers: UInt64,
+    lastCardIndex: UInt64,
+}) {
+    nextTurn() {
+        this.curPlayerIndex = this.curPlayerIndex.add(1).mod(this.maxPlayers);
+    }
+}
+
+class GameIndex extends Struct({
+    gameId: UInt64,
+    index: UInt64,
+}) {}
+
+function forceOptionValue<T>(o: Option<T>): T {
+    assert(
+        o.isSome,
+        'forceOptionValue fail. Trying to access unitialized value'
+    );
+    return o.value;
+}
+
 @runtimeModule()
-export class Fool extends MatchMaker {
-    @state() public games = StateMap.from<UInt64, UInt64>(UInt64, UInt64);
+export class Poker extends MatchMaker {
+    @state() public games = StateMap.from<UInt64, GameInfo>(UInt64, GameInfo);
 
     @state() public gamesNum = State.from<UInt64>(UInt64);
+
+    // // Session => player amount
+    // @state() public playersNum = StateMap.from<UInt64, UInt64>(UInt64, UInt64);
+
+    // [Session, index] => player
+    @state() public players = StateMap.from<GameIndex, PublicKey>(
+        GameIndex,
+        PublicKey
+    );
+
+    // [GameId, cardIndex] => player
+    @state() public kardToPlayer = StateMap.from<GameIndex, PublicKey>(
+        GameIndex,
+        PublicKey
+    );
+
+    public override initGame(
+        opponentReady: Bool,
+        opponent: Option<QueueListItem>
+    ): UInt64 {
+        // TODO
+        return UInt64.from(0);
+    }
+
+    @runtimeMethod()
+    public setup(
+        gameId: UInt64,
+        newDeck: EncryptedDeck,
+        shuffleProof: Proof<any, any>
+    ) {
+        let game = forceOptionValue(this.games.get(gameId));
+        // Check that game in setup status
+        assert(game.status.equals(UInt64.from(GameStatus.SETUP)));
+
+        let currentPlayer = this.getUserByIndex(gameId, game.curPlayerIndex);
+
+        // Check if right player runing setup
+        assert(currentPlayer.equals(this.transaction.sender));
+
+        // Check shuffle proof
+        // #TODO check, that public input equals previous deck
+        // Check that number of encryption increased
+        shuffleProof.verify();
+
+        // Update deck in games
+        game.deck = newDeck;
+        game.nextTurn();
+        game.status = Provable.if(
+            newDeck.numOfEncryptions.equals(game.maxPlayers),
+            UInt64.from(GameStatus.GAME),
+            game.status
+        );
+
+        this.games.set(gameId, game);
+    }
+
+    @runtimeMethod()
+    public drawCard() {}
+
+    private getUserByIndex(gameId: UInt64, index: UInt64): PublicKey {
+        return forceOptionValue(
+            this.players.get(
+                new GameIndex({
+                    gameId,
+                    index,
+                })
+            )
+        );
+    }
 }
